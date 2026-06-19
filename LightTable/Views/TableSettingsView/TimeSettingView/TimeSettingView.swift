c15d026109887b18cd13262d620c4480746362c0
yangpixi <2602340701@qq.com>
2026-06-19 15:21:49 +0800

    feat: support add/delete period

--- LightTable/Views/TableSettingsView/TimeSettingView/TimeSettingView.swift
+++ LightTable/Views/TableSettingsView/TimeSettingView/TimeSettingView.swift
@@ -4,12 +4,34 @@ import SwiftData
 struct TimeSettingView: View {
     @Query(sort: \Period.period, order: .forward) private var periods: [Period]
     
+    @Environment(\.modelContext) private var modelContext: ModelContext
+    private let viewModel = TimeSettingViewModel()
+    
     var body: some View {
         List {
-            ForEach(periods) { period in
+            ForEach(Array(periods.enumerated()), id: \.offset) { index, period in
                 PeriodSettingRow(period: period)
+                    .swipeActions(edge: .trailing) {
+                        if index == periods.count - 1 {
+                            Button(role: .destructive) { 
+                                viewModel.deleteItem(period)
+                            }
+                        }
+                    }
             }
         }
+        .onAppear {
+            viewModel.injectContext(modelContext: modelContext)
+        }
+        .toolbar(content: { 
+            ToolbarItem(placement: .topBarTrailing) { 
+                Button { 
+                    viewModel.addItem()
+                } label: { 
+                    Image(systemName: "plus")
+                }
+            }
+        })
         .navigationTitle("课表时间设置")
         .navigationBarTitleDisplayMode(.inline)
         .toolbarVisibility(.hidden, for: .tabBar)
